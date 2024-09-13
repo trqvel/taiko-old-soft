5f65f21e43a21c5ccf9370e81cd39f41ece7a2dd
travel <levqrt@gmail.com>
2024-09-13 19:53:15 +0300

    Add files via upload

---
+++ start.ts
@@ -0,0 +1,22 @@
+import { bridgeHandler, swapHandler, nftTaiko, fabricContract, minPad } from "./helper/logic";
+import { taiko_addons, smart_fabric } from "./setting";
+
+if (taiko_addons.bridge) {
+    bridgeHandler();
+};
+  
+if (taiko_addons.swap) {
+    swapHandler()
+};
+  
+if (taiko_addons.nft) {
+    nftTaiko()
+};
+  
+if (taiko_addons.smart_contracts) {
+    fabricContract(smart_fabric.number_contracts);
+};
+
+if (taiko_addons.minpad) {
+    minPad();
+};
